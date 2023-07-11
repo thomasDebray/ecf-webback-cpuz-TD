6ed9a410949f25860b7d43846a5d7f9136c273b3
CRM202A <mdevoldere@users.noreply.github.com>
2023-07-11 11:10:55 +0200

    Initial commit

---
+++ CpuApiSln/CpuApi/Db/CpuDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CpuApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CpuApi.Db
+{
+    public class CpuDbContext : DbContext
+    {
+        public DbSet<CpuModel> Cpus { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=db_cpus");
+        }
+    }
+}
---
+++ CpuApiSln/CpuApi/Models/CpuModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CpuApi.Models
+{
+    [Table("cpus")]
+    public class CpuModel
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
